8430b7e969a097f17f291c7a72bd707a63c23867
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-17 09:16:55 +0900

    Update Happy-Ladybugs.cpp

--- Happy-Ladybugs.cpp
+++ Happy-Ladybugs.cpp
@@ -1,77 +1,76 @@
 string happyLadybugs(string b)
 {
-	int c = std::count(b.begin(), b.end(), '_');
-	int check = 0;
-	int n = b.size() - c;
+    int c = std::count(b.begin(), b.end(), '_');
+    int check = 0;
+    int n = b.size() - c;
 
-	if (c != 0)
-	{
-		std::sort(b.begin(), b.end());
-	}
+    if (c != 0)
+    {
+        std::sort(b.begin(), b.end());
+    }
 
-	for (int i = 0; i < n; ++i)
-	{
-		if (b[i] == b[i-1] || b[i] == b[i+1])
-		{
-			check++;
-		}
-	}
+    for (int i = 0; i < n; ++i)
+    {
+        if (b[i] == b[i-1] || b[i] == b[i+1])
+        {
+            check++;
+        }
+    }
 
-	if (check == n)
-	{
-		return "YES";
-	}
-	else
-	{
-		return "NO";
-	}
+    if (check == n)
+    {
+        return "YES";
+    }
+    else
+    {
+        return "NO";
+    }
 }
-
 
 string happyLadybugs1(string b)
 {
-	int n = b.size();
-	int c = std::count(b.begin(), b.end(), '_');
-	int check = 0;
+    int n = b.size();
+    int c = std::count(b.begin(), b.end(), '_');
+    int check = 0;
 
-	if (c == 0)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			if (b[i] == b[i - 1] || b[i] == b[i + 1])
-			{
-				check++;
-			}
-		}
+    if (c == 0)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            if (b[i] == b[i - 1] || b[i] == b[i + 1])
+            {
+                check++;
+            }
+        }
 
-		if (check == n)
-		{
-			return "YES";
-		}
-		else
-		{
-			return "NO";
-		}
-	}
-	else
-	{
-		std::sort(b.begin(), b.end());
+        if (check == n)
+        {
+            return "YES";
+        }
+        else
+        {
+            return "NO";
+        }
+    }
+    else
+    {
+        std::sort(b.begin(), b.end());
 
-		for (int i = 0; i < n - c; ++i)
-		{
-			if (b[i] == b[i - 1] || b[i] == b[i + 1])
-			{
-				check++;
-			}
-		}
+        for (int i = 0; i < n - c; ++i)
+        {
+            if (b[i] == b[i - 1] || b[i] == b[i + 1])
+            {
+                check++;
+            }
+        }
 
-		if (check == n - c)
-		{
-			return "YES";
-		}
-		else
-		{
-			return "NO";
-		}
-	}
+        if (check == n - c)
+        {
+            return "YES";
+        }
+        else
+        {
+            return "NO";
+        }
+    }
 }
