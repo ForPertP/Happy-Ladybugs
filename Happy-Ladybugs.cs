dc38b5ffdf20d159c9c6400e1f67504028e73f90
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-10-17 18:51:19 +0900

    Update Happy-Ladybugs.cs

--- Happy-Ladybugs.cs
+++ Happy-Ladybugs.cs
@@ -1,1 +1,39 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int g = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int gItr = 0; gItr < g; gItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string b = Console.ReadLine();
+
+            string result = Result.happyLadybugs(b);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
 
