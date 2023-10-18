f229ad27c9874f5c15959fc79c25106f8b02ef77
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-10-19 00:40:12 +0900

    Update Happy-Ladybugs.cs

--- Happy-Ladybugs.cs
+++ Happy-Ladybugs.cs
@@ -12,6 +12,62 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
+class Result
+{
+
+    /*
+     * Complete the 'happyLadybugs' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING b as parameter.
+     */
+
+    public static string happyLadybugs(string b)
+    {
+        //List<int> x = new List<int>(new int[26]);
+        List<int> x = Enumerable.Repeat(0, 26).ToList();
+
+        bool sp = false;
+        int count = 0;
+        char d = '!';
+
+        foreach (char c in b)
+        {
+            if (c == '_')
+            {
+                sp = true;
+                continue;
+            }
+
+            if (++x[c - 'A'] == 1)
+            {
+                count++;
+            }
+
+            if (d != c)
+            {
+                count--;
+            }
+
+            d = c;
+        }
+
+        if (!sp && count != 0)
+        {
+            return "NO";
+        }
+
+        foreach (int i in x)
+        {
+            if (i == 1)
+            {
+                return "NO";
+            }
+        }
+
+        return "YES";
+    }
+}
 
 class Solution
 {
@@ -36,4 +92,3 @@ class Solution
         textWriter.Close();
     }
 }
-
